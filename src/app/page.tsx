2adf7ddf5d43510e05cc5557fa4fe80069a5fb15
carlostuuling <71810387+carlostuuling@users.noreply.github.com>
2023-06-16 23:02:30 +0300

    title size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#E5B0CE] items-center justify-between p-24">
       <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"></link>
-      <h1 className="font-bebas-neue">Welcome To Elysias Room</h1>
+      <h1 className="font-bebas-neue text-5xl">Welcome To Elysias Room</h1>
       <iframe
         width="560"
         height="315"
