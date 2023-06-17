bc8d3d8bbaba77355391061eab002d8d325dfc53
Owarice <carlos.tuuling22@gmail.com>
2023-06-17 18:12:06 +0300

    icon change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Learn about Elysia',
   description: 'small little site about Elysia',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    
     <html lang="en">
+      <Head>
+        <a rel="icon" href="/favicon.ico" />
+        <title>{metadata.title}</title>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
